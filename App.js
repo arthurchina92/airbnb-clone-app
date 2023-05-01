2fc3279bf3b1e1f810279567b461d3a38136c637
arthurchina92 <arthurchinaa@gmail.com>
2023-05-01 11:05:38 -0600

    First commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+function App() {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>hello world</Text>
+      </SafeAreaView>
+    </>
+  );
+}
+
+export default App;
